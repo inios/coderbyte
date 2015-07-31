5709371323675dd0899164cf5d76acc064d4e44f
inios <kjongwona@naver.com>
2015-07-31 22:26:16 +0900

    Create SecondGreatLow.rb

---
+++ SecondGreatLow.rb
@@ -0,0 +1,11 @@
+def SecondGreatLow(arr)
+
+  # code goes here
+  arr = [arr.uniq.sort[1], arr.uniq.sort[2]].join(" ")
+  return arr 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SecondGreatLow(STDIN.gets)  
