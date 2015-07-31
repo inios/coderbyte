e61246d232054fd5d0c0fdca8733911f52d8820c
inios <kjongwona@naver.com>
2015-07-31 21:23:28 +0900

    Create TimeConvert.rb

---
+++ TimeConvert.rb
@@ -0,0 +1,13 @@
+def TimeConvert(num)
+
+  # code goes here
+  hour = num/60
+  min = num%60
+  time = hour.to_s + ":" + min.to_s
+  return time 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+TimeConvert(STDIN.gets)  
